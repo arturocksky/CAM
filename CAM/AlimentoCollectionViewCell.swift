9440c0513899b1b31ab23585c08126d15be1f934
Arturo <arturo@Arturos-Mac.local>
2018-08-31 13:16:32 -0700

    Collection 2

---
+++ CAM/AlimentoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlimentoCollectionViewCell.swift
+//  CAM
+//
+//  Created by Arturo on 8/27/18.
+//  Copyright © 2018 cam. All rights reserved.
+//
+
+import UIKit
+
+class AlimentoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var outLabelAlimento: UILabel!
+    @IBOutlet weak var outImageAlimento: UIImageView!
+}
---
+++ CAM/AlimentosCollectionViewController.swift
@@ -0,0 +1,107 @@
+//
+//  AlimentosCollectionViewController.swift
+//  CAM
+//
+//  Created by Arturo on 8/27/18.
+//  Copyright © 2018 cam. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "reuseIdentifier"
+
+class AlimentosCollectionViewController: UICollectionViewController {
+    
+    let Alimentos:[String] = ["Manzana","Naranja","Pera","Guayaba","Melon","Papaya","Sandia","Kiwi","Platano","Tuna","Aguacate"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+       // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return Alimentos.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! AlimentoCollectionViewCell
+        
+        cell.outLabelAlimento.text = Alimentos[indexPath.row]
+        cell.outImageAlimento.image = #imageLiteral(resourceName: "apple.png")
+        // Configure the cell
+    
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "CollectionReusableView", for: indexPath) 
+            // do any programmatic customization, if any, here
+            return view
+        }
+        fatalError("Unexpected kind")
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
