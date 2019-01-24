8123af2710cbf602457980d6d3460fb959b09386
Arturo <arturo@Arturos-Mac.local>
2019-01-24 15:56:17 -0800

    completando...

---
+++ CAM/TransporteCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TransporteCollectionViewCell.swift
+//  CAM
+//
+//  Created by Arturo on 1/12/19.
+//  Copyright © 2019 cam. All rights reserved.
+//
+
+import UIKit
+
+class TransporteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var outLabelTransporte: UILabel!
+    @IBOutlet weak var outImageTransporte: UIImageView!
+}
